f92645831e05cd6a1aebcf263239cf89d159a769
Jim Yang <jsyang.ca@gmail.com>
2011-05-31 01:19:28 -0400

    changed stuff, added link tagging toy

--- linkAPI/server.js
+++ linkAPI/server.js
@@ -43,59 +43,6 @@ function GetRSS(port,host,path)
     });
 }
 
-// Get prices from a Kijiji results page.
-function PageGetPrices(xml)
-{    
-    var pricesOnPage=[];
-    
-    // fuck hardcore parsing libraries --- FOR NOW
-    // we'll do it live!
-    while(1)
-    {
-        // Make sure our dollar signs are in the <title>
-        var i=xml.indexOf("<title>");
-        if(i==-1) break;
-        xml=xml.slice(i+7);
-        
-        // Find the dollar sign.
-        var i=xml.indexOf("$");
-        if(i==-1) break;
-        xml=xml.slice(i+1);
-        
-        var j=xml.indexOf("</");
-        var listingPrice=parseFloat(xml.substring(0,j));
-        if(!isNaN(listingPrice)) pricesOnPage.push(listingPrice);
-        xml=xml.slice(j+8);
-    }
-    
-    var itemLastDate=xml.substring(xml.lastIndexOf("<dc:date>")+9,xml.lastIndexOf("</dc:date>"));
-    //console.log(pricesOnPage.length);
-    
-    return {
-        pricesOnPage:pricesOnPage,
-        itemLastDate:itemLastDate
-    };
-}
-
-// Get prices page from Kijiji.
-function PricesKijiji(item,region,port)
-{
-    var host=region+".kijiji.ca";
-    // 0 = all ads
-    // 1 = wanted
-    // 2 = offering
-    
-    // Min Price of 1 so we don't get "Please Contact" garbage.
-    var path="/f-SearchAdRss?AdType="+2+"&CatId=0&Keyword="+item+"&minPrice=1&Page=";
-    
-    port.prices=[]; // Add a field in there for the collected data (thus far).
-    port.page=1;    // Start from page 1, end when the date for the first item matches that of the last page...
-    port.itemLastDate="";
-    
-    GetRSS(port,host,path);
-
-}
-
 // Response writing.
 http.Server(function(req,res)
 {
@@ -107,9 +54,7 @@ http.Server(function(req,res)
 
     // Region first, then item name.
     var info=url.parse(req.url).pathname.split("/");
-    var region=info[1];
-    var item=info[2];
-    
+
     PricesKijiji(item,region,res);
     
 }).listen(8888);
